7e7451458f8bb934f631ea0fbce14477f4f41974
Cathyrt <cathrinert20@icloud.com>
2023-03-20 06:39:17 -0700

    Updated files

--- models/__init__.py
+++ models/__init__.py
@@ -4,6 +4,7 @@ import os
 from models.engine.file_storage import FileStorage
 from models.engine.db_storage import DBStorage
 
+
 if os.getenv("HBNB_TYPE_STORAGE") == "db":
     storage = DBStorage()
 else:
