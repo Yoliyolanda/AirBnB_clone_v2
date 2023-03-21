ebb424157b540b7975d92fb62070317f35ac97e3
Cathyrt <cathrinert20@icloud.com>
2023-03-21 04:06:12 -0700

    Update Review.py

--- models/review.py
+++ models/review.py
@@ -1,31 +1,21 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-import models
-from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String
+import os
+from sqlalchemy import Column, ForeignKey, String
 from sqlalchemy.orm import relationship
-from sqlalchemy import ForeignKey
+
+from models.base_model import BaseModel, Base
 
 
 class Review(BaseModel, Base):
-    """Representation of Review """
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'reviews'
-        text = Column(String(1024),
-                      nullable=False)
-        place_id = Column(String(60),
-                          ForeignKey('places.id'),
-                          nullable=False)
-        user_id = Column(String(60),
-                         ForeignKey('users.id'),
-                         nullable=False)
-    else:
-        text = ""
-        place_id = ""
-        user_id = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes Review"""
-        super().__init__(*args, **kwargs)
+    """ Review classto store review information """
+    __tablename__ = 'reviews'
+    place_id = Column(
+        String(60), ForeignKey('places.id'), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    user_id = Column(
+        String(60), ForeignKey('users.id'), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
+    text = Column(
+        String(1024), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
