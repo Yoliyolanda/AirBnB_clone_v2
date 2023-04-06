617db2b25e3292128d004a5bba8e1269bd0ce24a
Yoliyolanda <ymugabe25@gmail.com>
2023-04-06 16:22:38 -0700

    do_deploy_web_static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,55 +1,30 @@
 #!/usr/bin/python3
 """This script that distributes an archive to your web servers"""
-from fabric.api import env, put, run, sudo
-from os.path import exists
+from fabric.api import *
+from datetime import datetime
 import os
 
-# Define the web servers to deploy to
 env.hosts = ['100.25.180.155', '100.26.173.28']
-# Set the username used for SSH authentication
 env.user = 'ubuntu'
-env.key_filename = '~/.ssh/school'
+env.key_filename = '~/.ssh/school' 
 
 def do_deploy(archive_path):
-        """Deploy web files to server
-        """
-        try:
-                if not (path.exists(archive_path)):
-                        return False
-
-                # upload archive
-                put(archive_path, '/tmp/')
-
-                # create target dir
-                timestamp = archive_path[-18:-4]
-                run('sudo mkdir -p /data/web_static/\
-releases/web_static_{}/'.format(timestamp))
-
-                # uncompress archive and delete .tgz
-                run('sudo tar -xzf /tmp/web_static_{}.tgz -C \
-/data/web_static/releases/web_static_{}/'
-                    .format(timestamp, timestamp))
-
-                # remove archive
-                run('sudo rm /tmp/web_static_{}.tgz'.format(timestamp))
-
-                # move contents into host web_static
-                run('sudo mv /data/web_static/releases/web_static_{}/web_static/* \
-/data/web_static/releases/web_static_{}/'.format(timestamp, timestamp))
-
-                # remove extraneous web_static dir
-                run('sudo rm -rf /data/web_static/releases/\
-web_static_{}/web_static'
-                    .format(timestamp))
-
-                # delete pre-existing sym link
-                run('sudo rm -rf /data/web_static/current')
-
-                # re-establish symbolic link
-                run('sudo ln -s /data/web_static/releases/\
-web_static_{}/ /data/web_static/current'.format(timestamp))
-        except:
-                return False
-
-        # return True on success
+    """ Deploys files to server"""
+    if not os.path.exists(archive_path):
+        return False
+    try:
+        put(archive_path, "/tmp/")
+        archive_filename = os.path.basename(archive_path)
+        archive_folder = "/data/web_static/releases/{}".format(
+            archive_filename.split(".")[0])
+        run("mkdir -p {}".format(archive_folder))
+        run("tar -xzf /tmp/{} -C {}".
+            format(archive_filename, archive_folder))
+        run("rm /tmp/{}".format(archive_filename))
+        run("mv {}/web_static/* {}/".format(archive_folder, archive_folder))
+        run("rm -rf {}/web_static".format(archive_folder))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(archive_folder))
         return True
+    except:
+        return False
